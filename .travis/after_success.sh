ef688417932b466baed62c1fc8a34eb187aad46d
Pablo Torres <pablo.torres@insight-centre.org>
2017-09-25 13:58:51 +0100

    set python version when uploading

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -5,6 +5,6 @@ set -x -e
 # Only publish when master is build
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   export PATH="$MINICONDA_ROOT_FOLDER/bin:$PATH"
-  file=$(conda build .conda/ --output)
+  file=$(conda build --python 3.5 .conda/ --output)
   anaconda -t $ANACONDA_TOKEN upload $file -u $ANACONDA_USER --force
 fi
